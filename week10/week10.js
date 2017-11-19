483ee97f00149fed89207fb90726b1af4714bac1
davisa94 <austin.i.davis94@gmail.com>
2017-11-18 20:18:48 -0700

    Added files for

--- week10/week10.js
+++ week10/week10.js
@@ -1,14 +1,14 @@
-function eventListener(elementId){
-   elementId.addEventListener
-}
-
 function loadFile(value, loadArea){
    var filename = value.toString();
    var xhttp = new XMLHttpRequest();
+   var response;
+   document.getElementById(loadArea).innerHTML = "winning";
    xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       if (this.responseText != null){
-         document.getElementById(loadArea).innerHTML = this.responseText;
+         response = this.responseText;
+         console.log(response);
+         document.getElementById(loadArea).innerHTML = response;
           }
     }
   };
