719e551780ad6d2552efceb84346f4d330a1b73e
davisa94 <austin.i.davis94@gmail.com>
2017-11-18 19:45:27 -0700

    Changed filename variable to strongly typed

--- week10/week10.js
+++ week10/week10.js
@@ -3,7 +3,7 @@ function eventListener(elementId){
 }
 
 function loadFile(value, loadArea){
-   var String filename = value;
+   String filename = value;
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
