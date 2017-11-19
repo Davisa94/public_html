ad656d4f7113a9cc3e4b82aa273b2a2ad2aa2e63
davisa94 <austin.i.davis94@gmail.com>
2017-11-18 20:21:59 -0700

    Cleaned up JavaScript error checking mess

--- week10/week10.js
+++ week10/week10.js
@@ -1,14 +1,11 @@
 function loadFile(value, loadArea){
    var filename = value.toString();
    var xhttp = new XMLHttpRequest();
-   var response;
    document.getElementById(loadArea).innerHTML = "winning";
    xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       if (this.responseText != null){
-         response = this.responseText;
-         console.log(response);
-         document.getElementById(loadArea).innerHTML = response;
+         document.getElementById(loadArea).innerHTML = this.responseText;
           }
     }
   };
