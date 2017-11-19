eceed2478fa2e7f74426b3d6ad636444db6ab2c1
davisa94 <austin.i.davis94@gmail.com>
2017-11-18 19:57:29 -0700

    dded some error handling

--- week10/week10.js
+++ week10/week10.js
@@ -7,7 +7,9 @@ function loadFile(value, loadArea){
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementById(loadArea).innerHTML = this.responseText;
+      if (this.responseText != NULL){
+         document.getElementById(loadArea).innerHTML = this.responseText;
+          }
     }
   };
   xhttp.open("GET", filename, true);
